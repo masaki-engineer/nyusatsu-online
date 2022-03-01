dfd600c2bcd27c7bb35337e2ddfbc549a4d396ec
masaki-engineer <gp77.am@gmail.com>
2022-03-01 16:15:47 +0900

    App.js作成

---
+++ app/javascript/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import styled from 'styled-components'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      Hello React! Hello React! Hello React! Hello React!
+    </>
+  )
+}
+
+export default App
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import { BrowserRouter } from 'react-router-dom';
 import App from '../components/App'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>,
+    document.querySelector('#root'),
+  );
+});
