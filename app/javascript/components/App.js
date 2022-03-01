3091bbab52d610b24420e74008cbc66508292bad
masaki-engineer <gp77.am@gmail.com>
2022-03-01 16:20:03 +0900

    Headerコンポーネント(空)の作成

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,9 +3,12 @@ import { Switch, Route, Link } from 'react-router-dom'
 import styled from 'styled-components'
 import './App.css'
 
+import Header from './Header'
+
 function App() {
   return (
     <>
+      <Header/>
       Hello React! Hello React! Hello React! Hello React!
     </>
   )
