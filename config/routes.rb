a376bc15c6351310b2625c8715c660818af81043
masaki-engineer <gp77.am@gmail.com>
2022-03-04 17:08:54 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'Municipality', at: 'auth'
+
   root to: 'site#index'
+
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'Municipality', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+      namespace :auth do
+        resources :sessions, only: [:index]
+      end
+    end
+  end
+  
 end
