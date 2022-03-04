9ae8fdf11f2ef8db1b7290b48de5d4c1df760272
masaki-engineer <gp77.am@gmail.com>
2022-03-04 18:22:46 +0900

    Companyモデル生成

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,13 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'Municipality', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
       }
+      mount_devise_token_auth_for 'Company', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
       namespace :auth do
         resources :sessions, only: [:index]
       end
     end
   end
-  
+
 end
