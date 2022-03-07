4ccdb3b381b94e044117b468b35d47cc3668c37f
masaki-engineer <gp77.am@gmail.com>
2022-03-07 17:12:52 +0900

    Routes設定

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import styled from 'styled-components'
 // import './App.css'
 
 import Header from './Header'
 import Footer from './Footer'
 import TopPage from './TopPage'
+import MunicipalitySignUp from './MunicipalitySignUp'
 
 function App() {
   return (
     <>
       <Header/>
-      <TopPage/>
+      <Routes>
+        <Route exact path="/" element={<TopPage />} />
+        <Route exact path="/municipality/sign_up" element={<MunicipalitySignUp />} />
+      </Routes>
       <Footer/>
     </>
   )
