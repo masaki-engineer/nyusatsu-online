92e199fa30a556c5e91cffcdb7384f55b8e87cd9
masaki-engineer <gp77.am@gmail.com>
2022-03-08 12:29:24 +0900

    municipalityのAPIメソッド定義

---
+++ app/javascript/components/src/lib/api/municipality.js
@@ -0,0 +1,31 @@
+import client from "lib/api/client"
+import Cookies from "js-cookie"
+
+// サインアップ（新規アカウント作成）
+export const signUp = (params) => {
+  return client.post("municipality", params)
+}
+
+// サインイン（ログイン）
+export const signIn = (params)  => {
+  return client.post("municipality/sign_in", params)
+}
+
+// サインアウト（ログアウト）
+export const signOut = () => {
+  return client.delete("municipality/sign_out", { headers: {
+    "access-token": Cookies.get("_access_token"),
+    "client": Cookies.get("_client"),
+    "uid": Cookies.get("_uid")
+  }})  
+}
+
+// 認証済みのユーザーを取得
+export const getCurrentUser = () => {
+  if (!Cookies.get("_access_token") || !Cookies.get("_client") || !Cookies.get("_uid")) return
+  return client.get("/auth/sessions", { headers: {
+    "access-token": Cookies.get("_access_token"),
+    "client": Cookies.get("_client"),
+    "uid": Cookies.get("_uid")
+  }})
+}
