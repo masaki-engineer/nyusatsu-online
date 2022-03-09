3c76576e3d0d7eb9a828e707c14cdb33965b0c91
masaki-engineer <gp77.am@gmail.com>
2022-03-09 18:06:49 +0900

    companyのAPIメソッド定義

---
+++ app/javascript/components/src/lib/api/company.js
@@ -0,0 +1,31 @@
+import client from "./client"
+import Cookies from "js-cookie"
+
+// サインアップ（新規アカウント作成）
+export const signUp = (params) => {
+  return client.post("company", params)
+}
+
+// サインイン（ログイン）
+export const signIn = (params)  => {
+  return client.post("company/sign_in", params)
+}
+
+// サインアウト（ログアウト）
+export const companySignOut = () => {
+  return client.delete("company/sign_out", { headers: {
+    "access-token": Cookies.get("_access_token"),
+    "client": Cookies.get("_client"),
+    "uid": Cookies.get("_uid")
+  }})  
+}
+
+// 認証済みのユーザーを取得
+export const getCurrentCompany = () => {
+  if (!Cookies.get("_access_token") || !Cookies.get("_client") || !Cookies.get("_uid")) return
+  return client.get("/company/sessions", { headers: {
+    "access-token": Cookies.get("_access_token"),
+    "client": Cookies.get("_client"),
+    "uid": Cookies.get("_uid")
+  }})
+}
