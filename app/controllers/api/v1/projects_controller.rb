971cd327fed4dd624b5c5203adaf119b4fe50bcb
masaki-engineer <gp77.am@gmail.com>
2022-03-14 21:53:56 +0900

    トップページに案件表示：レスポンスのデータ加工メソッド追加

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ProjectsController < ApplicationController
   def recent
-    projects = Project.order("created_at DESC").limit(5)
+    projects = convert_for_front(
+      Project.order("created_at DESC").limit(5)
+    )
     render json: projects
   end
 
@@ -20,5 +22,28 @@ class Api::V1::ProjectsController < ApplicationController
       :name, :category_id, :overview, :qualification, :bid_date,
       :rep_division, :rep_person, :phone_number, :email, :url, :municipality_id
     )
+  end
+
+  def convert_for_front(projects)
+    converted_projects = []
+    projects.each do |project|
+      converted_project = {
+      name: project.name,
+      category_id: project.category_id,
+      overview: project.overview,
+      qualification: project.qualification,
+      bid_date: project.bid_date,
+      rep_division: project.rep_division,
+      rep_person: project.rep_person,
+      phone_number: project.phone_number,
+      email: project.email,
+      url: project.url,
+      municipality_id: project.municipality_id,
+      municipality_name: Municipality.find(project.municipality_id)[:name],
+      create_date: project.created_at.to_date
+      }
+      converted_projects.push(converted_project)
+    end
+    return converted_projects
   end
 end
