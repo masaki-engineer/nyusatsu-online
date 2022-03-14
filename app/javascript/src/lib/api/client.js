12a8c96def317d4058fd6f8cd0d6c688444191a9
masaki-engineer <gp77.am@gmail.com>
2022-03-14 22:29:03 +0900

    baseURLを本番環境用に書き換え

--- app/javascript/src/lib/api/client.js
+++ app/javascript/src/lib/api/client.js
@@ -7,7 +7,7 @@ const options = {
 }
 
 const client = applyCaseMiddleware(axios.create({
-  baseURL: "http://localhost:3000/api/v1"
+  baseURL: "http://175.41.230.223/api/v1"
 }), options)
 
 export default client
