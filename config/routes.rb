cb835ad6f66225d6dcf2b16d8d114451a6e306a3
masaki-engineer <gp77.am@gmail.com>
2022-03-16 21:52:15 +0900

    municipality/:id ルートの作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   get 'projects/new', to: 'site#index'
   get 'projects/search', to: 'site#index'
+  get 'municipality/:id', to: 'site#index'
 
   namespace :api do
     namespace :v1 do
