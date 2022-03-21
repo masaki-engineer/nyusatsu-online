906c9761d62e640c2d1698ea400d312c9ebf3610
masaki-engineer <gp77.am@gmail.com>
2022-03-21 10:28:49 +0900

    bidsテーブルのマイグレーションファイルの編集

--- db/migrate/20220321012413_create_bids.rb
+++ db/migrate/20220321012413_create_bids.rb
@@ -1,7 +1,13 @@
 class CreateBids < ActiveRecord::Migration[6.1]
   def change
     create_table :bids do |t|
-
+      t.string :rep_division, null: false
+      t.string :rep_person,   null: false
+      t.string :phone_number, null: false
+      t.string :email,        null: false
+      t.integer :price,       null: false
+      t.references :company,  null: false, foreign_key: true
+      t.references :project,  null: false, foreign_key: true
       t.timestamps
     end
   end
