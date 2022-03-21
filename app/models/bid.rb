b59fee9c50c3f2ef36df300ba595d921ca4fcb3f
masaki-engineer <gp77.am@gmail.com>
2022-03-21 21:10:18 +0900

    bidsモデルのバリデーション設定

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -4,6 +4,7 @@ class Bid < ApplicationRecord
   validates :phone_number, presence: true
   validates :email,        presence: true
   validates :price,        presence: true
+  validates :project_id, uniqueness: { scope: :company_id }
 
   belongs_to :project
   belongs_to :company
