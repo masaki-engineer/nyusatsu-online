99d05682b1a5dba68a7c69c07c2a1d5c30c364c4
masaki-engineer <gp77.am@gmail.com>
2022-03-21 18:23:51 +0900

    successesテーブルのマイグレーションファイルの編集

--- db/migrate/20220321092118_create_successes.rb
+++ db/migrate/20220321092118_create_successes.rb
@@ -1,7 +1,9 @@
 class CreateSuccesses < ActiveRecord::Migration[6.1]
   def change
     create_table :successes do |t|
-
+      t.references :company, null: false, foreign_key: true
+      t.references :project, null: false, foreign_key: true
+      t.references :bid,     null: false, foreign_key: true
       t.timestamps
     end
   end
