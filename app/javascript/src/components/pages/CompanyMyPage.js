993242191d7e91da7ff7be6a02f450c39a8f6a56
masaki-engineer <gp77.am@gmail.com>
2022-03-23 11:22:49 +0900

    企業：見た目を作成

--- app/javascript/src/components/pages/CompanyMyPage.js
+++ app/javascript/src/components/pages/CompanyMyPage.js
@@ -1,36 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 
+import { AuthContext } from "../../App"
 import Button from '../utils/Button'
 import UnderlineText from '../utils/UnderlineText'
 import SearchForm from '../utils/SearchForm'
+import Project from '../../components/utils/Project'
+import { searchProjects } from "../../lib/api/project"
 
 const AllContents = styled.div`
 background: #ffffff;
 width: 1200px;
 margin: 0 auto;
 padding: 10px 0;
-display: flex;
 `
 
-const SideBar = styled.div`
-width: 300px;
-border-right: solid 1px gray;
-padding: 0 15px;
-`
-
-const Content = styled.div`
-width: 900px;
+const Header = styled.div`
+width: 100%;
 padding: 0 15px;
 `
 
 const ControlBox = styled.div`
-background: #ffffff;
+background: #f0f8ff;
 height: 230px;
-width: 840px;
-border-radius: 10px;
-border: solid 1px #e7e7e7;
-box-shadow: 2px 2px 10px 0px #dcdcdc;
+width: 100%;
 margin: 20px auto;
 padding: 30px;
 display: flex;
@@ -44,40 +38,69 @@ border-right: solid 1px gray;
 
 const StatusBox = styled.div`
 height: 100%;
-padding: 20px 0 20px 50px;
+padding-left: 50px;
+display: flex;
 `
 
-const Row = styled.a`
-display: flex;
-width: 100%;
-height: 30px;
-margin: 0 auto 15px auto;
+const Row = styled.div`
+width: 200px;
+padding: 20px;
+margin: auto 0;
 `
 
 const Status = styled.div`
 color: #0156a5;
 font-size: 25px;
 font-weight: bold;
-width: 130px;
+text-align: center;
 `
 
 const Count = styled.div`
-font-size: 15px;
+font-size: 25px;
 height: 100%;
+text-align: center;
+font-weight: bold;
 `
 
 const Number = styled.span`
-font-size: 25px;
+font-size: 50px;
+`
+
+const Contents = styled.div`
+width: 100%;
+display: flex;
+`
+
+const SideBar = styled.div`
+width: 300px;
+border-right: solid 1px gray;
+padding: 0 15px;
+`
+
+const Content = styled.div`
+width: 900px;
+`
+
+const Projects = styled.div`
+width: 860px;
+margin: 20px;
 `
 
 function CompanyMyPage() {
+  const { currentCompany } = useContext(AuthContext)
+  const id = currentCompany.id
+  const [projects, setProjects] = useState([])
+  const query = useLocation().search
+
+  useEffect(async () => {
+    const res = await searchProjects(query)
+    const bidProjects = res.data.filter(project => project.bids.map(bid => bid.companyId).includes(currentCompany.id))
+    setProjects(bidProjects)
+  }, [query])
+
   return (
     <AllContents>
-      <SideBar>
-        <UnderlineText text={'案件を検索する'} />
-        <SearchForm/>
-      </SideBar>
-      <Content>
+      <Header>
         <UnderlineText text={'◯◯株式会社　マイページ'} />
         <ControlBox>
 
@@ -88,21 +111,14 @@ function CompanyMyPage() {
 
           <StatusBox>
             <Row href="#">
-              <Status>受付中</Status>
+              <Status>落札結果待ち</Status>
               <Count>
                 <Number>100</Number> 件
               </Count>
             </Row>
 
             <Row href="#">
-              <Status>入札済み</Status>
-              <Count>
-                <Number>100</Number> 件
-              </Count>
-            </Row>
-
-            <Row href="#">
-              <Status>結果あり</Status>
+              <Status>落札結果あり</Status>
               <Count>
                 <Number>100</Number> 件
               </Count>
@@ -110,7 +126,22 @@ function CompanyMyPage() {
           </StatusBox>
 
         </ControlBox>
-      </Content>
+      </Header>
+      <Contents>
+        <SideBar>
+          <UnderlineText text={'絞り込み'} />
+          <SearchForm/>
+        </SideBar>
+        <Content>
+          <Projects>
+            {projects.map((project) => {
+              return (
+                <Project project={project} />
+              )
+            })}
+          </Projects>
+        </Content>
+      </Contents>
     </AllContents>
   )
 }
